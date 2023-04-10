dc45c5ce15e7bc1107ef45818c2170ce61eba26d
NullVoidNan <128657358+QuixoticLeprechauns@users.noreply.github.com>
2023-04-10 16:27:43 -0700

    Stuff Happening

--- pfts_loop.h
+++ pfts_loop.h
@@ -1,5 +1,24 @@
 #ifndef PFTS_LOOP_H
 #define PFTS_LOOP_H
-#include <threads.h>
+
+#include <windows.h>
+static DWORD WINAPI sleep_for(LPVOID lpParam)
+{
+    Sleep(*((int*)lpParam));
+}
+
+void pfts_loop(int (*process) (void), int MaxSleepTime, int MinSleepTime)
+{
+    int retval;
+    do
+    {
+        HANDLE ts = CreateThread(NULL, 0, sleep_for, &MaxSleepTime, 0, NULL);
+        retval = process();
+        Sleep(MinSleepTime);
+
+        WaitForSingleObject(ts, INFINITE);
+        CloseHandle(ts);
+    } while (retval != 0);
+}
 
 #endif
