8be21f0ba1d3d7d1e9c4968101c1f89cf290e6ae
NullVoidNan <128657358+QuixoticLeprechauns@users.noreply.github.com>
2023-04-13 10:55:21 -0700

    Update pfts_loop.h

--- pfts_loop.h
+++ pfts_loop.h
@@ -3,27 +3,32 @@
 
 #include <windows.h>
 
+typedef int (*loop_process) (void);
 static DWORD WINAPI sleep_for(LPVOID lpParam)
 {
-    Sleep(*((int*)lpParam));
+    Sleep(*(int*)lpParam);
 }
 
 /// @brief Runs a process in a passive fixed time step loop. 
 /// @param process A function returning int without arguments. Return 0 to exit loop.
 /// @param MaxSleepTime The target fixed time step, in miliseconds, for this loop.
 /// @param MinSleepTime The minimum period of time, in miliseconds, this loop will sleep between cycles.
-void pfts_loop(int (*process) (void), int MaxSleepTime, int MinSleepTime)
+void pfts_loop(loop_process process, int MaxSleepTime, int MinSleepTime)
 {
     int retval;
+
     do
     {
         HANDLE ts = CreateThread(NULL, 0, sleep_for, &MaxSleepTime, 0, NULL);
+
         retval = process();
+
         Sleep(MinSleepTime);
 
         WaitForSingleObject(ts, INFINITE);
         CloseHandle(ts);
     } while (retval != 0);
+    
 }
 
 #endif
